b92b76f1869dfa7d0d30fddc2c22d1039b4e2641
nr-rahul <60250753+nr-rahul@users.noreply.github.com>
2025-12-10 13:34:47 -0500

    Create Reservation.java

---
+++ src/main/java/model/Reservation.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Reservation {
+
+    private String customerName;
+    private Flight flight;
+    private int seatsBooked;
+
+    public Reservation(String customerName, Flight flight, int seatsBooked) {
+        this.customerName = customerName;
+        this.flight = flight;
+        this.seatsBooked = seatsBooked;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public int getSeatsBooked() {
+        return seatsBooked;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "customerName='" + customerName + '\'' +
+                ", flight=" + flight +
+                ", seatsBooked=" + seatsBooked +
+                '}';
+    }
+}
